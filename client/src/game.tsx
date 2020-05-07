884d4ec1a01c0b6500d566a853e4d235984f322f
a-rts <a-rts@users.noreply.github.com>
2020-05-07 08:57:02 +0100

    Implemented wiring for ArraySchema and combined it  with the implementation for MapSchema.

--- client/src/game.tsx
+++ client/src/game.tsx
@@ -26,59 +26,76 @@ async function joinGame() {
   game.room = room;
 }
 
-function setValue<T, K extends keyof T>(getValue: ()=> T[K], shadowState: T, field: K) {
+function setValue<T, K extends keyof T>(
+  getValue: () => T[K],
+  shadowState: T,
+  field: K
+) {
   const value = getValue();
   if (value instanceof Schema) {
     // TODO make more robust guard
     if (!shadowState[field]) {
       runInAction(() => {
-        shadowState[field] = { ...value};
+        shadowState[field] = { ...value };
       });
       wireSchemaChanges(getValue as any, shadowState[field]);
     }
-  } else if (value instanceof ArraySchema) {
-    // TODO The onAdd and onRemove functions are the same as for MapSchema
-    //  but shadowState[field] must be [].
-    // TODO Test having Schema/ArraySchema/MapSchema within ArraySchema.
-  } else if (value instanceof MapSchema) {
+  } else if (value instanceof MapSchema || value instanceof ArraySchema) {
     if (!shadowState[field]) {
       runInAction(() => {
-        shadowState[field] = { ...value }; 
+        if (value instanceof ArraySchema) {
+          shadowState[field] = [...value] as any; // TODO Type
+        } else {
+          shadowState[field] = { ...value };
+        }
       });
-      wireMapChanges(getValue as any, shadowState[field]);
+      wireCollectionChanges(getValue as any, shadowState[field]);
     }
-    value.triggerAll(); // Do not miss the first element. 
-    // TODO Test having Schema/ArraySchema/MapSchema within MapSchema.
-  } else { //primitive value
+    value.triggerAll(); // Do not miss the first element.
+    // TODO Test having Schema/ArraySchema/MapSchema within ArraySchema and MapSchema.
+  } else {
+    // Set a primitive value.
     runInAction(() => {
       shadowState[field] = value;
     });
   }
 }
 
-function wireMapChanges<T>(getColyseusState: () => (MapSchema & T), shadowState: T) {
+function wireCollectionChanges<T>(
+  getColyseusState: () => (MapSchema | ArraySchema) & T,
+  shadowState: T
+) {
   const colyseusState = getColyseusState();
-  colyseusState.onAdd = (_, k) => {
+  colyseusState.onAdd = (_: any, k: string | number) => {
     const key = k as keyof T;
     setValue(() => getColyseusState()[key], shadowState, key);
   };
-  colyseusState.onChange = (_, k) => {
-    // maybe not an actual change in direct value? need to test with Schema value
+  colyseusState.onChange = (_: any, k: string | number) => {
+    // TODO maybe not an actual change in direct value? need to test with Schema value
     const key = k as keyof T;
     setValue(() => getColyseusState()[key], shadowState, key);
   };
-  colyseusState.onRemove = (_, k) => {
+  colyseusState.onRemove = (_: any, k: string | number) => {
     const key = k as keyof T;
-    delete shadowState[key];
+    if (colyseusState instanceof ArraySchema) {
+      // TODO Fix type to allow ArraySchema.splice().
+      // @ts-ignore: Property 'splice' does not exist on type 'T'.ts(2339)
+      shadowState.splice(key, 1);
+    } else {
+      delete shadowState[key];
+    }
   };
 }
 
-function wireSchemaChanges<T>(getColyseusState:  () => (Schema & T), shadowState: T) {
+function wireSchemaChanges<T>(
+  getColyseusState: () => Schema & T,
+  shadowState: T
+) {
   const colyseusState = getColyseusState();
   colyseusState.onChange = (changes) => {
     for (const change of changes) {
       const field = change.field as keyof T;
-      if (getColyseusState()[field] === undefined){
+      if (getColyseusState()[field] === undefined) {
         delete shadowState[field];
       } else {
         setValue(() => getColyseusState()[field], shadowState, field);
